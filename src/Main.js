1c131e807c02ffb9d200ea4f2bc15a89e6be4544
royce79-creator <connorsboyce07@gmail.com>
2021-08-07 17:30:55 -0700

    Made changes to the front end to have the info for lab 07

--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,9 @@ import './Main.css';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-
+import Weather from './weather.js';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 class Main extends React.Component {
   constructor(props) {
     super(props);
@@ -16,6 +18,8 @@ class Main extends React.Component {
       displayMap: false,
       displayError: false,
       errorMsg: '',
+      displayWeather:false,
+      weatherData: [],
     }
   }
 
@@ -34,7 +38,7 @@ class Main extends React.Component {
 
       this.setState ({
         showCityName: true,
-        city: cityMap.data[0].display_name,
+        cityDisplay: cityMap.data[0].display_name,
         lat: cityMap.data[0].lat,
         lon: cityMap.data[0].lon,
         displayMap: true,
@@ -48,13 +52,29 @@ class Main extends React.Component {
       })
       console.log('this error', error.message);
     }
+    this.handleWeatherCity();
   }
-
-
-
+    handleWeatherCity = async () => {
+      try {
+        let weatherCity = await axios.get(`http://localhost:3001/weather?searchQuery=${this.state.city}`)
+        
+        // console.log(weatherCity);
+        this.setState({
+          displayWeather: true,
+          weatherData: weatherCity.data,
+        })
+      }
+      catch (error) {
+        this.setState({
+          displayError: true,
+          errorMsg: `Error has occured: ${error.response.status}, ${error.response.data.error}`
+        })
+        console.log('this error 2', error.message);
+      }
+    }
 
   render() {
-    console.log(this.state.city);
+    // console.log(this.state.city);
     return(
       <div>
         <h1>Welcome to City Explorer, search to your heart's content!</h1>
@@ -73,6 +93,12 @@ class Main extends React.Component {
           {this.state.lon ? <h2>Lon: {this.state.lon}</h2> : ''}
           {this.state.displayMap ? <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_APIKEY}&center=${this.state.lat},${this.state.lon}&zoom=13`} alt={this.state.city} /> : ''}
           {this.state.errorMsg ? <h2>{this.state.errorMsg}</h2> : ''}
+          {this.state.weatherData.length ? <Card style={{width: '18rem'}}>
+            <Card.Header>The Temperatures</Card.Header>
+              <ListGroup variant="flush">
+              <ListGroup.Item>{this.state.displayWeather ? <Weather weatherData={this.state.weatherData}/> : ''}</ListGroup.Item>
+              </ListGroup>
+          </Card> : ''}
         </Container>
       </div>
 
